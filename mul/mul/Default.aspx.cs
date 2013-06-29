015d4bea5ea6ec1a4f2e620cc54ab758bec98f7c
wzquan <w.z.quan@163.com>
2013-06-29 17:06:46 +0800

    1

---
+++ mul/mul/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace mul
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("<p><font color=\"blue\" size=\"5\">99乘法口诀表</font></p><p></p>");
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Response.Write(j + "x" + i + "=" + i * j + " ");
+                }
+                Response.Write("<br>");
+            }
+
+        }
+    }
+}
